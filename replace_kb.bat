5088632125f1f71e28c66912e9d5ce6b07a3a7df
Bhavesh163 <bhaveshfung@gmail.com>
2025-11-07 22:22:20 +0700

    Deleted useless files

--- replace_kb.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-python -c "import shutil; shutil.copy(r'C:\cblue-ai\common\knowledge_base.py', r'C:\cblue-ai\common\knowledge_base_backup.py'); print('Backup created'); shutil.copy(r'C:\cblue-ai\common\knowledge_base_new.py', r'C:\cblue-ai\common\knowledge_base.py'); print('Replacement complete')"
